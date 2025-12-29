1e93e10de1633e850d4ab3fa9419f5d9ea4251c2
Hao Liu <v2top1@163.com>
2025-12-29 13:32:34 -0800

    Fix: dependencies resolution by pinning to specific revisions

--- Package.swift
+++ Package.swift
@@ -21,9 +21,9 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/relatedcode/ProgressHUD.git", from: "14.1.3"),
-        .package(url: "https://github.com/lyleLH/VisualEffectView.git", branch: "master"),
-        .package(url: "https://github.com/lyleLH/TwemojiKit.git", branch: "master"),
-        .package(url: "https://github.com/SVGKit/SVGKit.git", branch: "3.x"),
+        .package(url: "https://github.com/lyleLH/VisualEffectView.git", revision: "3cc067c6fd75e805f83f8b17ccb572dffffa2384"),
+        .package(url: "https://github.com/lyleLH/TwemojiKit.git", revision: "ccb794c6888a373474075d58035a4382116bec8a"),
+        .package(url: "https://github.com/SVGKit/SVGKit.git", revision: "6137e27e6a0b24587967d97aeb25a6706f8eb036"),
         .package(url: "https://github.com/SDWebImage/SDWebImage.git", from: "5.18.10")
     ],
     targets: [
